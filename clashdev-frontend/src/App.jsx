1856fc2cc4088f8fdd0656acc6496c90f393d779
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-09-09 01:12:03 +0530

    initialzing the project with vite build tool

---
+++ clashdev-frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import WelcomePage from "./pages/WelcomePage";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<WelcomePage />} />
+
+          {/* <Route pah="/home" element ={<HomePage/>}/> */}
+          {/* <Route path="/signin" element={<SignIn />} /> */}
+          {/* <Route path="/signup" element={<SignUp />} /> */}
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
