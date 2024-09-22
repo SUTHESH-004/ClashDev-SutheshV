159d8946647a7c277264345d06fac018052fef0c
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-09-22 13:49:09 +0530

    added content in liveChallenge

--- clashdev-frontend/src/components/LiveChallege.jsx
+++ clashdev-frontend/src/components/LiveChallege.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const LiveChallege = () => {
-  return (
-    <div>LiveChallege</div>
-  )
-}
+  const value = 1;
+  if (value === 1)
+    return (
+      <div>
+        <p>Live</p> <span className="h-1 w-1 bg-blue-700 rounded-full"></span>
+      </div>
+    );
+  else if (value === 2)
+    return (
+      <>
+        <div className=" h-[100%] w-[100%] flex justify-center items-center text-2xl">
+          Currently No Challenges Right Now ..!
+        </div>
+      </>
+    );
+};
 
-export default LiveChallege
+export default LiveChallege;
