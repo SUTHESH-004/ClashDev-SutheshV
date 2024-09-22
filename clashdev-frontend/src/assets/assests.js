25b1f2241a27f244a8216bd347544a66ef97d568
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-09-22 13:47:22 +0530

    add profile photo

--- clashdev-frontend/src/assets/assests.js
+++ clashdev-frontend/src/assets/assests.js
@@ -1,8 +1,12 @@
 import header from "./header-bg.jpg";
 import PvsM from "./bg-pekkavsmega.jpg";
 import pkvmg from "./pkbmgbg.jpg";
+import buttonbg from "./button-bg-mobile@2x.037a540.jpg";
+import profile from "./dummy-profile-pic.jpg";
 export const assets = {
   header,
   PvsM,
   pkvmg,
+  buttonbg,
+  profile,
 };
