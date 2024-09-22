5183de58be7ed6a48b09897de7d7a4f1235f2ce9
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-09-22 13:49:30 +0530

    added route to  welcome page

--- clashdev-frontend/src/App.jsx
+++ clashdev-frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import WelcomePage from "./pages/WelcomePage";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/welcome" element={<WelcomePage />} />
           {/* <Route pah="/home" element ={<HomePage/>}/> */}
           {/* <Route path="/signin" element={<SignIn />} /> */}
           {/* <Route path="/signup" element={<SignUp />} /> */}
